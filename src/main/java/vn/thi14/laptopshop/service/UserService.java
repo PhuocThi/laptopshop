cb992a96c58c6e5e7ae9133d6fddcced70515edf
Thi14 <phuocthi2002@gmail.com>
2025-02-08 09:21:55 +0700

    demo mvc

---
+++ src/main/java/vn/thi14/laptopshop/service/UserService.java
@@ -0,0 +1,10 @@
+package vn.thi14.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello(){
+        return "hello from service";
+    }
+}
